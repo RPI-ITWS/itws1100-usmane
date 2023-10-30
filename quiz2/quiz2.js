ae3e9155c591d555a9c5ba602ac83148e5118373
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2023-10-30 19:52:13 -0400

    quiz2

---
+++ quiz2/quiz2.js
@@ -0,0 +1,8 @@
+import data from './quiz2.json' assert {type: 'json '};
+console.log(data);
+
+
+function() {
+    console.log(musicList[(Math.floor(Math.random() * musicList.length))])
+}
+
---
+++ quiz2/readMe.txt
@@ -0,0 +1,4 @@
+GitHub id: Emmanuelusman2004
+repo name: itws1100-usmane
+azure homepagelink: https://usmanerpi.eastus.cloudapp.azure.com/iit
+Discord Handle: GrumpyUnknown#4982
