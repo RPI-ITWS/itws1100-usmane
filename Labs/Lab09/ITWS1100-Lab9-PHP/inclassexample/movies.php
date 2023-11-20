ce11296dfad1e246faf140d309eaef1e89ebee79
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2023-11-20 14:26:43 -0500

    made movies

--- Labs/Lab09/ITWS1100-Lab9-PHP/inclassexample/movies.php
+++ Labs/Lab09/ITWS1100-Lab9-PHP/inclassexample/movies.php
@@ -10,10 +10,152 @@
       
 <?php include('includes/menubody.inc.php'); ?>
 
-<p>Build the movie forms and output here.</p>
+<?php
+  // We'll need a database connection both for retrieving records and for
+  // inserting them.  Let's get it up front and use it for both processes
+  // to avoid opening the connection twice.  If we make a good connection,
+  // we'll change the $dbOk flag.
+  $dbOk = false;
 
+  /* Create a new database connection object, passing in the host, username,
+     password, and database to use. The "@" suppresses errors. */
+  @ $db = new mysqli('localhost', 'phpmyadmin', 'Loveshadow12!', 'iit');
 
+  if ($db->connect_error) {
+    echo '<div class="messages">Could not connect to the database. Error: ';
+    echo $db->connect_errno . ' - ' . $db->connect_error . '</div>';
+  } else {
+    $dbOk = true;
+  }
 
-<?php include('includes/foot.inc.php'); 
+  // Now let's process our form:
+  // Have we posted?
+  $havePost = isset($_POST["save"]);
+
+  // Let's do some basic validation
+  $errors = '';
+  if ($havePost) {
+
+    // Get the output and clean it for output on-screen.
+    // First, let's get the output one param at a time.
+    // Could also output escape with htmlentities()
+    $mid = htmlspecialchars(trim($_POST["movieid"]));
+    $tit = htmlspecialchars(trim($_POST["title"]));
+    $yr = htmlspecialchars(trim($_POST["year"]));
+
+    $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
+
+    if ($mid == '') {
+      $errors .= '<li>movieid may not be blank</li>';
+      if ($focusId == '') $focusId = '#movieid';
+    }
+    if ($tit == '') {
+      $errors .= '<li>Title may not be blank</li>';
+      if ($focusId == '') $focusId = '#title';
+    }
+    if ($yr == '') {
+      $errors .= '<li>year may not be blank</li>';
+      if ($focusId == '') $focusId = '#year';
+    }
+
+    if ($errors != '') {
+      echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
+      echo $errors;
+      echo '</ul></div>';
+      echo '<script type="text/javascript">';
+      echo '  $(document).ready(function() {';
+      echo '    $("' . $focusId . '").focus();';
+      echo '  });';
+      echo '</script>';
+    } else {
+      if ($dbOk) {
+        // Let's trim the input for inserting into mysql
+        // Note that aside from trimming, we'll do no further escaping because we
+        // use prepared statements to put these values in the database.
+        $movieForDb = trim($_POST["movieid"]);
+        $titleForDb = trim($_POST["title"]);
+        $yearForDb = trim($_POST["year"]);
+
+        // Setup a prepared statement. Alternately, we could write an insert statement - but
+        // *only* if we escape our data using addslashes() or (better) mysqli_real_escape_string().
+        $insQuery = "insert into movies (`movieid`,`title`,`year`) values(?,?,?)";
+        $statement = $db->prepare($insQuery);
+        // bind our variables to the question marks
+        $statement->bind_param("iss",$movieForDb,$titleForDb,$yearForDb);
+        // make it so:
+        $statement->execute();
+
+        // give the user some feedback
+        echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' movie added to database.</h4>';
+        echo $movieid . ' ' . $title . ', made ' . $year . '</div>';
+
+        // close the prepared statement obj
+        $statement->close();
+      }
+    }
+  }
+?>
+
+<h3>Add Movie</h3>
+<form id="addForm" name="addForm" action="index.php" method="post" onsubmit="return validate(this);">
+  <fieldset>
+    <div class="formData">
+
+      <label class="field" for="Movieid">Movie(id):</label>
+      <div class="value"><input type="int" size="100" value="<?php if($havePost && $errors != '') { echo $movieid; } ?>" name="movieid" id="movieid"/></div>
+
+      <label class="field" for="title">title:</label>
+      <div class="value"><input type="text" size="100" value="<?php if($havePost && $errors != '') { echo $title; } ?>" name="title" id="title"/></div>
+
+      <label class="field" for="year">year:</label>
+      <div class="value"><input type="text" size="10" maxlength="10" value="<?php if($havePost && $errors != '') { echo $year; } ?>" name="year" id="year"/> <em>yyyy</em></div>
+
+      <input type="submit" value="save" id="save" name="save"/>
+    </div>
+  </fieldset>
+</form>
+
+<h3>Movies</h3>
+<table id="movieTable">
+<?php
+  if ($dbOk) {
+
+    $query = 'select * from movies order by id';
+    $result = $db->query($query);
+    $numRecords = $result->num_rows;
+
+    echo '<tr><th>movieid:</th><th>title:</th><th></th></tr>';
+    for ($i=0; $i < $numRecords; $i++) {
+      $record = $result->fetch_assoc();
+      if ($i % 2 == 0) {
+        echo "\n".'<tr id="movie-' . $record['movieid'] . '"><td>';
+      } else {
+        echo "\n".'<tr class="odd" id="movie-' . $record['movieid'] . '"><td>';
+      }
+      echo htmlspecialchars($record['movieid']) . ', ';
+      echo htmlspecialchars($record['title']);
+      echo '</td><td>';
+      echo htmlspecialchars($record['year']);
+      echo '</td><td>';
+      echo '<img src="resources/delete.png" class="deletemovie" width="16" height="16" alt="delete movie"/>';
+      echo '</td></tr>';
+      // Uncomment the following three lines to see the underlying
+      // associative array for each record.
+      // echo '<tr><td colspan="3" style="white-space: pre;">';
+      // print_r($record);
+      // echo '</td></tr>';
+    }
+
+    $result->free();
+
+    // Finally, let's close the database
+    $db->close();
+  }
+
+?>
+</table>
+
+<?php include('includes/foot.inc.php');
   // footer info and closing tags
 ?>
+
