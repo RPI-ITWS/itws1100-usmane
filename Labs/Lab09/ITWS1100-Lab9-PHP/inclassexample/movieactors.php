c606cfbbd1ffb8b1c42f5611e5e7d88fcc65882d
Emmanuelusman2004 <117528761+Emmanuelusman2004@users.noreply.github.com>
2023-11-20 18:16:57 -0500

    Update movieactors.php

--- Labs/Lab09/ITWS1100-Lab9-PHP/inclassexample/movieactors.php
+++ Labs/Lab09/ITWS1100-Lab9-PHP/inclassexample/movieactors.php
@@ -1,18 +1,13 @@
-<?php
+<?php 
   include('includes/init.inc.php'); // include the DOCTYPE and opening tags
   include('includes/functions.inc.php'); // functions
-  error_reporting(E_ALL);
-  ini_set('display_errors', '1');
 ?>
-<title>PHP &amp; MySQL - ITWS</title>
+<title>PHP &amp; MySQL - ITWS</title>   
 
-<?php
-  include('includes/head.inc.php');
-  // include global css, javascript, end the head and open the body
-?>
+<?php include('includes/head.inc.php'); ?>
 
 <h1>PHP &amp; MySQL</h1>
-
+      
 <?php include('includes/menubody.inc.php'); ?>
 
 <?php
@@ -33,45 +28,106 @@
     $dbOk = true;
   }
 
-  
+  // Now let's process our form:
+  // Have we posted?
+  $havePost = isset($_POST["save"]);
+
+  // Let's do some basic validation
+  $errors = '';
+  if ($havePost) {
+
+    // Get the output and clean it for output on-screen.
+    // First, let's get the output one param at a time.
+    // Could also output escape with htmlentities()
+    $movieid = htmlspecialchars(trim($_POST["movieid"]));
+    $actorid = htmlspecialchars(trim($_POST["actorid"]));
+
+    $focusId = ''; // trap the first field that needs updating, better would be to save errors in an array
+
+    if ($movieid == '') {
+      $errors .= '<li>movieid may not be blank</li>';
+      if ($focusId == '') $focusId = '#movieid';
+    }
+    if ($actorid == '') {
+      $errors .= '<li>actorid may not be blank</li>';
+      if ($focusId == '') $focusId = '#actorid';
+    }
+
+    if ($errors != '') {
+      echo '<div class="messages"><h4>Please correct the following errors:</h4><ul>';
+      echo $errors;
+      echo '</ul></div>';
+      echo '<script type="text/javascript">';
+      echo '  $(document).ready(function() {';
+      echo '    $("' . $focusId . '").focus();';
+      echo '  });';
+      echo '</script>';
+    } else {
       if ($dbOk) {
+        // Let's trim the input for inserting into MySQL
+        // Note that aside from trimming, we'll do no further escaping because we
+        // use prepared statements to put these values in the database.
+        $movieidForDb = trim($_POST["movieid"]);
+        $actoridForDb = trim($_POST["actorid"]);
+
+        // Setup a prepared statement. Alternately, we could write an insert statement - but
+        // *only* if we escape our data using addslashes() or (better) mysqli_real_escape_string().
+        $insQuery = "INSERT INTO movieactors (`movieid`,`actorid`) VALUES (?,?)";
+        $statement = $db->prepare($insQuery);
+        // bind our variables to the question marks
+        $statement->bind_param("ii", $movieidForDb, $actoridForDb);
+        // make it so:
+        $statement->execute();
+
+        // give the user some feedback
+        echo '<div class="messages"><h4>Success: ' . $statement->affected_rows . ' record added to the database.</h4>';
+        echo $movieid . ' ' . $actorid . '</div>';
 
         // close the prepared statement obj
         $statement->close();
       }
-    
-  
+    }
+  }
 ?>
 
+<h3>Add Movie Actor</h3>
+<form id="addForm" name="addForm" action="index.php" method="post" onsubmit="return validate(this);">
+  <fieldset>
+    <div class="formData">
 
+      <label class="field" for="movieid">Movie ID:</label>
+      <div class="value"><input type="text" size="60" value="<?php if($havePost && $errors != '') { echo $movieid; } ?>" name="movieid" id="movieid"/></div>
 
-<h3>Movies & Actors</h3>
-<table id="Movies&ActorsTable">
+      <label class="field" for="actorid">Actor ID:</label>
+      <div class="value"><input type="text" size="60" value="<?php if($havePost && $errors != '') { echo $actorid; } ?>" name="actorid" id="actorid"/></div>
+
+      <input type="submit" value="save" id="save" name="save"/>
+    </div>
+  </fieldset>
+</form>
+
+<h3>Movie Actors</h3>
+<table id="movieActorTable">
 <?php
   if ($dbOk) {
 
-    $query = 'select * from movieactors order by movieid';
+    $query = 'SELECT * FROM movieactors ORDER BY movieid';
     $result = $db->query($query);
     $numRecords = $result->num_rows;
 
-    echo '<tr><th>movieid:</th><th>actorid:</th><th></th></tr>';
+    echo '<tr><th>Movie ID:</th><th>Actor ID:</th><th></th></tr>';
     for ($i=0; $i < $numRecords; $i++) {
       $record = $result->fetch_assoc();
       if ($i % 2 == 0) {
-        echo "\n".'<tr id="actor-' . $record['actorid'] . '"><td>';
+        echo "\n".'<tr id="movieactor-' . $record['movieid'] . '"><td>';
       } else {
-        echo "\n".'<tr class="odd" id="actor-' . $record['actorid'] . '"><td>';
+        echo "\n".'<tr class="odd" id="movieactor-' . $record['movieid'] . '"><td>';
       }
-      echo htmlspecialchars($record['actorid']) . ', ';
-      echo htmlspecialchars($record['movieid']);
+      echo htmlspecialchars($record['movieid']) . '</td><td>';
+      echo htmlspecialchars($record['actorid']);
       echo '</td><td>';
-      echo '<img src="resources/delete.png" class="deleteActor" width="16" height="16" alt="delete actor"/>';
+      echo '<img src="resources/delete.png" class="deletemovieactor" width="16" height="16" alt="delete movie actor"/>';
       echo '</td></tr>';
-      // Uncomment the following three lines to see the underlying
-      // associative array for each record.
-      // echo '<tr><td colspan="3" style="white-space: pre;">';
-      // print_r($record);
-      // echo '</td></tr>';
     }
 
     $result->free();
@@ -79,7 +135,6 @@
     // Finally, let's close the database
     $db->close();
   }
-
 ?>
 </table>
 
